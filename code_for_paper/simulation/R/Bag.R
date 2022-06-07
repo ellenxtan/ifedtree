1c4b876da8159d2b73ef363f17f2e9f5827edbd8
XQ <38412452+ellenxtan@users.noreply.github.com>
2022-06-07 17:16:59 -0400

    update sim code

--- code_for_paper/simulation/R/Bag.R
+++
@@ -1,9 +0,0 @@
-## Bagging (directly augment tau_hat from individual model)
-Bag <- function(df, ind_all) {
-    bag_est <- mean(ind_all$tau_hat, na.rm=TRUE)
-    df[, tau_hat := bag_est]
-    
-    bag_out <- df[, c("R", "S", "tau", "tau_hat")]
-    
-    return(bag_out)
-}
--- code_for_paper/simulation/R/LstToDF.R
+++
@@ -1,6 +0,0 @@
-## Transform nested lists to dataframe (data.table)
-LstToDF <- function(lst, df_name) {
-    out_lst <- purrr::map(lst, df_name)
-    out_df <- purrr::map_dfr(out_lst, rbind)  # row-bind
-    return(out_df)
-}
